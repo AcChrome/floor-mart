78009dbcba192c23a5b3ae6b6a9a48692cb95113
AcChrome <ac.chan@live.com>
2022-09-13 20:07:35 -0700

    updated cardcontent for product

--- src/component/products/product/Product.jsx
+++ src/component/products/product/Product.jsx
@@ -9,10 +9,26 @@ import {
 } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} />
+      <CardMedia className={classes.media} title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography variant="h5" gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography variant="h5">{product.price}</Typography>
+        </div>
+        <Typography variant="h2" color="textSecondary">
+          {product.description}
+        </Typography>
+      </CardContent>
+      <CardAction disableSpacing className={classes.cardACtions}>
+        <IconButton aria-label="Add to Cart">
+          <AddShoppingCart />
+        </IconButton>
+      </CardAction>
     </Card>
   );
 };
