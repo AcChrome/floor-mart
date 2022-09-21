264e3e0fe225b57da9376172fa073221c3500f59
AcChrome <ac.chan@live.com>
2022-09-20 22:53:10 -0700

    fixed badgeContent synxtax

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           <div className={classes.grow} />
           <div className={classes.button}>
             <IconButton aria-label="show cart items" color="inherit">
-              <Badge badgetContent={2} color="secondary">
+              <Badge badgeContent={2} color="secondary">
                 <ShoppingCart />
               </Badge>
             </IconButton>
