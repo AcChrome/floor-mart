766e7a986e4e1e79590cb3b62fc1006f9a9d1074
AcChrome <ac.chan@live.com>
2022-09-22 21:55:01 -0700

    initial commit using commercejs for backend api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import { commerce } from "./lib/commerce";
 import { Products, Navbar } from "./component";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
   return (
     <div>
       <Navbar />
