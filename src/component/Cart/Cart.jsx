f1eae4c2f26f02f75da60eecdcc530055c758972
AcChrome <ac.chan@live.com>
2022-11-08 22:36:37 -0800

    trying to fix empty tag error

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -4,7 +4,6 @@ import { Container, Typography, Button, Grid } from "@material-ui/core";
 import useStyles from "./styles";
 
 const Cart = ({ cart }) => {
-  const isEmpty = !cart.line_items.length;
   const classes = useStyles();
 
   const EmptyCart = () => (
@@ -49,13 +48,16 @@ const Cart = ({ cart }) => {
       </Grid>
     </>
   );
+
+  if (!cart.line_items) return "Loading...";
+
   return (
     <Container>
       <div className={classes.toolbar} />
       <typography className={classes.title} variant="h3">
         Your Shopping Cart
       </typography>
-      {isEmpty ? <EmptyCard /> : <FilledCart />}
+      {!cart.line_items.length ? <EmptyCard /> : <FilledCart />}
     </Container>
   );
 };
