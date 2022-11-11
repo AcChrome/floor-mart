9f9d6c3f0925d32df853df03f2f9892f1ab89b7b
AcChrome <ac.chan@live.com>
2022-11-10 23:24:51 -0800

    added cardcontent

--- src/component/Cart/Cartitem/Cartitem.jsx
+++ src/component/Cart/Cartitem/Cartitem.jsx
@@ -11,11 +11,24 @@ import {
 import useStyles from "./styles";
 import { InsertEmoticon } from "@material-ui/icons";
 
-const Cartitem = () => {
+const Cartitem = ({ item }) => {
   const classes = useStyles();
   return (
     <Card>
-      <CardMedia image={InsertEmoticon.media.source} />
+      <CardMedia
+        image={InsertEmoticon.media.source}
+        alt={item.name}
+        className={classes.media}
+      />
+      <CardContent className={classes.CardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <div className={classes.buttons}></div>
+      </CardActions>
     </Card>
   );
 };
